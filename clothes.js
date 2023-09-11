de41077b957ca5a77cca6678d6f4bbe104627584
Zola <zolboo@alumni.stanford.edu>
2023-09-10 19:31:58 -0700

    temp

--- clothes.js
+++ clothes.js
@@ -1,6 +1,7 @@
-const urls = ['https://www.uniqlo.com/jp/ja/contents/feature/lifewear-collection/common_23fw/img/collage/productSub/productSub_460711_30.png', 
-'https://www.uniqlo.com/jp/ja/contents/feature/lifewear-collection/common_23fw/img/collage/productSub/productSub_460914_12.png']
+// const urls = ['https://www.uniqlo.com/jp/ja/contents/feature/lifewear-collection/common_23fw/img/collage/productSub/productSub_460711_30.png',
+// 'https://www.uniqlo.com/jp/ja/contents/feature/lifewear-collection/common_23fw/img/collage/productSub/productSub_460914_12.png']
 
+const urls = ['./assets/product1.png', './assets/product2.png'];
 const url =
   'https://www.uniqlo.com/jp/ja/contents/feature/lifewear-collection/common_23fw/img/collage/productSub/productSub_460914_12.png';
 const options = {
@@ -11,11 +12,11 @@ const options = {
 };
 
 (async () => {
-  try { 
+  try {
     for (i = 0; i < 2; i++) {
       const response = await fetch(urls[i], options);
       const result = await response.blob();
-      const objectURL = URL.createObjectURL(result); 
+      const objectURL = URL.createObjectURL(result);
       let myImage;
       if (i === 0) {
         myImage = document.querySelector('#test');

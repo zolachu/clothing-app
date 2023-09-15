cb2784e3d8287054ed56450d31d6572cd09a5aa4
Zola <zolboo@alumni.stanford.edu>
2023-09-14 19:15:08 -0700

    hover effect to guns

--- shrink-ray.js
+++ shrink-ray.js
@@ -8,3 +8,19 @@ shrinkRayEl.addEventListener('click', (e) => {
   const deg2 = 20 - deg;
   //   shrinkRayEl.style.setProperty('--tilt-degree', `${deg2}`);
 });
+
+const boxes = document.querySelectorAll('.light-container .box');
+setInterval(bounce, 10000);
+
+function bounce() {
+  boxes.forEach((box) => {
+    for (let i = 0; i < 4; i++) {
+      setTimeout(() => {
+        box.style.boxShadow = '0 0 30px #E0FBC2';
+      }, 500);
+      setTimeout(() => {
+        box.style.boxShadow = 0;
+      }, 500);
+    }
+  });
+}
